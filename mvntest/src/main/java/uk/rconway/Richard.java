7ab877d17145fd82c1303ef21243b034b623804c
Richard Conway <richard@rconway.co.uk>
2012-02-02 21:12:09 +0000

    Import package mvntest

---
+++ mvntest/src/main/java/uk/rconway/Richard.java
@@ -0,0 +1,13 @@
+package uk.rconway;
+
+import org.apache.log4j.Logger;
+
+public class Richard
+{
+	public static Logger logger = Logger.getLogger(Richard.class);
+
+	public static void main(String[] args)
+	{
+		logger.info("This is Richard - built with Maven");
+	}
+}
