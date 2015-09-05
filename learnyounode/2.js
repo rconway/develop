2a2bff58ecdb2252e64cbe4a43086d32eda97755
Richard Conway <richard@rconway.co.uk>
2015-09-05 11:20:55 +0100

    Added official answer to Exercise 2

--- learnyounode/2.js
+++ learnyounode/2.js
@@ -1,6 +1,6 @@
-var answer = 0;
-for (var i = 2; i < process.argv.length; ++i)
-{
-	answer += Number(process.argv[i]);
-}
-console.log(answer);
+var result = 0
+
+for (var i = 2; i < process.argv.length; i++)
+	result += Number(process.argv[i])
+
+console.log(result)
