c8ea5f4616b9dd5c431d62794d179502a8f9433b
Richard Conway <richard@rconway.co.uk>
2015-09-05 11:26:50 +0100

    Added official answer to Exercise 3

--- learnyounode/3.js
+++ learnyounode/3.js
@@ -1,9 +1,10 @@
-var fs = require('fs');
+var fs = require('fs')
 
-var buffer = fs.readFileSync(process.argv[2]);
+var contents = fs.readFileSync(process.argv[2])
+var lines = contents.toString().split('\n').length - 1
+console.log(lines)
 
-var bufferStr = buffer.toString();
-
-var parts = bufferStr.split('\n');
-
-console.log(parts.length-1);
+// note you can avoid the .toString() by passing 'utf8' as the
+// second argument to readFileSync, then you'll get a String!
+//
+// fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
