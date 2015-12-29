6089a29794ae396168633116f3642956445c1892
Richard Conway <richard@rconway.co.uk>
2015-12-29 15:36:43 +0000

    Added official answer to Exercise 7

--- learnyounode/7.js
+++ learnyounode/7.js
@@ -1,17 +1,7 @@
-var http = require("http");
-
-var url = process.argv[2];
-
-http.get(url, function(response)
-{
-	response.setEncoding("utf8");
-	
-	response.on("data", console.log);
-	
-	response.on("error", console.error);
-	
-	response.on("end", function(data)
-	{
-		// Do nothing
-	});
-});
+var http = require('http')
+    
+http.get(process.argv[2], function (response) {
+	response.setEncoding('utf8')
+	response.on('data', console.log)
+	response.on('error', console.error)
+})
