27de0d7be0a3ab23b72e2b42a543427126de3b38
Richard Conway <richard@rconway.co.uk>
2015-12-29 15:04:49 +0000

    Added official answer to Exercise 5

--- learnyounode/5.js
+++ learnyounode/5.js
@@ -1,24 +1,9 @@
-var fs = require('fs');
-
-fs.readdir(process.argv[2], processDir);
-
-function processDir(err, list)
-{
-	for (var i = 0; i < list.length; ++i)
-	{
-		printIfMatchExt(list[i], process.argv[3]);
-	}
-}
-
-function printIfMatchExt(str, ext)
-{
-	var parts = str.split('.');
-	if (parts.length > 1)
-	{
-		var lastPart = parts[parts.length-1];
-		if (lastPart == ext)
-		{
-			console.log(str);
-		}
-	}
-}
+var fs = require('fs')
+var path = require('path')
+    
+fs.readdir(process.argv[2], function (err, list) {
+	list.forEach(function (file) {
+		if (path.extname(file) === '.' + process.argv[3])
+			console.log(file)
+	})
+})
